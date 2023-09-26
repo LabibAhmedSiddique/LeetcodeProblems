b9088a9c581a49aa4b63deafa8d1807208c05c86
LabibAhmedSiddique <labibahmedsiddique.0326@gmail.com>
2023-09-26 12:00:19 +0600

    Time: 65 ms (66.18%), Space: 17.6 MB (29.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {}  # val -> index
-
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i
+        check={}
+        for i,n in enumerate(nums):
+            #check the difference 
+            diff=target-n
+            #if that difference is in hashmap return the index and i which is the current pointer 
+            if diff in check :
+                return [check[diff],i]
+            #na hole oi index err value and the index it is assigned to will be created in the hashmap    
+            check[n]=i    
+                
+        
+                      
+        
