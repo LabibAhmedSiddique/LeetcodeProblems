96e38654af1cba63900b60a5fc8850792f8280cb
LabibAhmedSiddique <labibahmedsiddique.0326@gmail.com>
2023-09-27 20:09:02 +0600

    Time: 99 ms (58.04%), Space: 21.2 MB (35.53%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,20 +1,13 @@
-from collections import Counter
-from itertools import islice
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
-
-        frequency = {}
-
-        for num in nums:
-
-            if num not in frequency:
-
-                frequency[num] = 1
-
-            else:
-
-                frequency[num] = frequency[num] + 1
-        frequency = dict(sorted(frequency.items(),key=lambda x: x[1],  reverse=True))
-        result=list(frequency.keys())[:k] 
-        return result
+        freq={}
+        for n in nums :
+            if n not in freq :
+                freq[n]=1
+            else :
+                freq[n]=freq[n]+1
+       
+        sorted_dict = {k: v for k, v in sorted(freq.items(), key=lambda item: item[1],reverse=True)}  
+        print(sorted_dict)
+        arr=list(sorted_dict)
+        return arr[0:k]
